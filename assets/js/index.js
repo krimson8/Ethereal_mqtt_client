05a099538f0556093926cb647db4d9c871082b31
krimson8 <krimson8@gmail.com>
2018-01-29 18:27:40 +0800

    fixed bugs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
-
 var ip_show = document.querySelector('#ip');
 var topic_show = document.querySelector('#topic');
 
@@ -6,10 +5,10 @@ var row1 = "";
 var row2 = "";
 var row3 = "";
 var main = document.querySelector('#rec');
-main.innerHTML = row1 + "<br>" + row2 + "<br>" + row3 + "<br>";
+main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
 
-var temp = document.querySelector('#temp');
-var humid = document.querySelector('#humid');
+// var temp = document.querySelector('#temp');
+// var humid = document.querySelector('#humid');
 var index_temp = 0;
 var index_humid = 0;
 var client;
@@ -38,8 +37,8 @@ function connect()
 			index_uv = row3.indexOf("UV");
 			index_soil = row3.indexOf("Soil");
 			index_press = row3.indexOf("Pressure");
-			index_status = row3.indexOf("Status");
-			index_status = row3.indexOf("Status")
+			index_status = row3.indexOf("Time");
+			console.log(index_status);
 
 			temp.value = row3.slice(index_temp + 12, index_temp + 17);
 			humid.value = row3.slice(index_humid + 9, index_humid + 14);
@@ -47,9 +46,9 @@ function connect()
 			uv.value = row3.slice(index_uv + 3, index_uv + 8);
 			soil.value = row3.slice(index_soil + 5, index_soil + 12);
 			press.value = row3.slice(index_press + 9, index_press + 16);
-			status.value = row3.slice(index_status + 5, index_status + 9);
+			stime.value = row3.slice(index_status + 5, index_status + 32);
 			//console.log( payload.toString() );
-			main.innerHTML = row1 + "<br>" + row2 + "<br>" + row3 + "<br>";
+			main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
 			//txt.value = payload.toString();
 			console.log( payload.toString() );
 			  
