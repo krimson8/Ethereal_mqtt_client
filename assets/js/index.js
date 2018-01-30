30cb2afe7c1d58e5219e66e3c583d7e7172ddc17
krimson8 <krimson8@gmail.com>
2018-01-30 19:41:46 +0800

    added status section

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,19 +5,35 @@ var row1 = "";
 var row2 = "";
 var row3 = "";
 var main = document.querySelector('#rec');
+var module = document.getElementById("module-id");
 main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
 
-// var temp = document.querySelector('#temp');
-// var humid = document.querySelector('#humid');
-var index_temp = 0;
-var index_humid = 0;
 var client;
 
 var IP;
 var topic;
+var m_value;
+m_value = module.options[module.selectedIndex].text
+
+function init()
+{
+	var row1 = "";
+	var row2 = "";
+	var row3 = "";
+	main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
+	m_value = module.options[module.selectedIndex].text
+	temp.value = "";
+	humid.value = "";
+	light.value = "";
+	uv.value = "";
+	soil.value = "";
+	press.value = "";
+	stime.value = "";
+}
 
 function connect()
 {
+	// m_value = module.options[module.selectedIndex].text
 	IP = ip_show.value;
 	topic = topic_show.value;
 	
@@ -26,30 +42,33 @@ function connect()
 	
 	client.on("message", 
 		function(topic, payload) 
-		{
+		{	
 			row1 = row2;
 			row2 = row3;
 			row3 = payload.toString();
 
-			index_temp = row3.indexOf("Temperature");
-			index_humid = row3.indexOf("Humidity");
-			index_light = row3.indexOf("Light");
-			index_uv = row3.indexOf("UV");
-			index_soil = row3.indexOf("Soil");
-			index_press = row3.indexOf("Pressure");
-			index_status = row3.indexOf("Time");
-			console.log(index_status);
-
-			temp.value = row3.slice(index_temp + 12, index_temp + 17);
-			humid.value = row3.slice(index_humid + 9, index_humid + 14);
-			light.value = row3.slice(index_light + 6, index_light + 11);
-			uv.value = row3.slice(index_uv + 3, index_uv + 8);
-			soil.value = row3.slice(index_soil + 5, index_soil + 12);
-			press.value = row3.slice(index_press + 9, index_press + 16);
-			stime.value = row3.slice(index_status + 5, index_status + 32);
-			//console.log( payload.toString() );
-			main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
-			//txt.value = payload.toString();
+			if(row3.slice(7, 10) == m_value) {
+				index_temp = row3.indexOf("Temperature");
+				index_humid = row3.indexOf("Humidity");
+				index_light = row3.indexOf("Light");
+				index_uv = row3.indexOf("UV");
+				index_soil = row3.indexOf("Soil");
+				index_press = row3.indexOf("Pressure");
+				index_status = row3.indexOf("Time");
+	
+				temp.value = row3.slice(index_temp + 12, index_temp + 17);
+				humid.value = row3.slice(index_humid + 9, index_humid + 14);
+				light.value = row3.slice(index_light + 6, index_light + 11);
+				uv.value = row3.slice(index_uv + 3, index_uv + 8);
+				soil.value = row3.slice(index_soil + 5, index_soil + 12);
+				press.value = row3.slice(index_press + 9, index_press + 16);
+				stime.value = row3.slice(index_status + 5, index_status + 32);
+				//console.log( payload.toString() );
+				main.innerHTML = row1 + "<br>" + "<br>" + row2 + "<br>" + "<br>" + row3 + "<br>" + "<br>";
+			}
+			else {
+				init();
+			}
 			console.log( payload.toString() );
 			  
 		}
